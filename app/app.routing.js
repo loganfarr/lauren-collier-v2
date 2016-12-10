c231cefe0cffdfe91389bc4c38cbf7520bd42c14
Logan Farr <Logan@loganfarr.com>
2016-12-10 15:46:24 -0700

    Added routing link for portfolio;

--- app/app.routing.js
+++ app/app.routing.js
@@ -1,7 +1,9 @@
 "use strict";
-var router_1 = require("@angular/router");
-var home_component_1 = require("./HomeComponent/home.component");
+var router_1 = require('@angular/router');
+var home_component_1 = require('./HomeComponent/home.component');
+var portfolio_component_1 = require('./PortfolioComponent/portfolio.component');
 exports.routing = router_1.RouterModule.forRoot([
-    { path: '', component: home_component_1.HomeComponent }
+    { path: '', component: home_component_1.HomeComponent },
+    { path: 'portfolio', component: portfolio_component_1.PortfolioComponent }
 ]);
 //# sourceMappingURL=app.routing.js.map
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,7 +1,9 @@
 import { Router, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './HomeComponent/home.component';
+import { PortfolioComponent } from './PortfolioComponent/portfolio.component';
 
 export const routing = RouterModule.forRoot([
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent }
 ]);
