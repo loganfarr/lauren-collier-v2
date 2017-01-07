dd1ec82c6f785e749328714179291f90af6aa21e
Logan Farr <logan@loganfarr.com>
2017-01-07 13:52:42 -0700

    Added album template files and styles. Adjusted component to pull from the AlbumService

--- app/AlbumComponent/album.component.ts
+++ app/AlbumComponent/album.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit }             from '@angular/core';
+import { Component, OnInit }     from '@angular/core';
 import { Router, ActivatedRoute} from '@angular/router';
+// import { Observable }            from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+
+import { AlbumService } from '../AlbumService/album.service';
 
 @Component({
 	selector: 'album',
@@ -8,13 +12,36 @@ import { Router, ActivatedRoute} from '@angular/router';
 })
 
 export class AlbumComponent implements OnInit {
-  albumName = '';
+  albumName: string = '';
+  photos: any = [];
+  header: any = [];
+
+  private viewingMode = 'thumbnails';
+
+  overlayActive = false;
 
   constructor(
     private _router: Router, 
-    private _ActivatedRoute: ActivatedRoute) {}
+    private _ActivatedRoute: ActivatedRoute,
+    private _AlbumService: AlbumService) {}
 
   ngOnInit() {
-    this._ActivatedRoute.params.subscribe(params => this.albumName = params['name']);
+    this._ActivatedRoute.params.subscribe(params => {
+      this.albumName = params['name'];
+
+      this._AlbumService.getPhotos(this.albumName).subscribe(res => {
+        this.header = res.header;
+        this.photos = res.photos;
+      });
+    });
+  }
+
+  changeViewingMode(mode: string) {
+    if(mode != 'thumbnails' && mode != 'list') {
+      alert('Invalid viewing mode');
+      return;
+    }
+
+    this.viewingMode = mode;
   }
 }
